66a8c5d358ce76fd233c6ae70844e7c6334f9c72
daniel lau <daniellautimao@gmail.com>
2022-10-10 21:04:39 -0300

    Add files via upload
    JAVA

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+var huginho = 10;
+
+var huginho2 = 9;
+
+var huginho3 = 8;
+
+var huginho4 = 10;
+
+console.log(
+    'media dos alunos sera' + (huginho + huginho2 + huginho3 + huginho4)
+);
+
+var media = huginho + huginho2 + huginho3 + huginho4 / 4;
+
+console.log(media);
